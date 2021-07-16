08bd401ecb738819aaf7abba5a82cc6232f0e49f
Mayumi <maythermyself@gmail.com>
2021-07-16 11:06:03 -0500

    pom pattern added

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -31,27 +31,27 @@ describe('Buy a t-shirt', () => {
   it('then should be bought a t-shirt', async () => {
     await browser.waitForAngularEnabled(false);
     await browser.get('http://automationpractice.com/');
-    await(browser.sleep(3000));
+    await(browser.sleep(10000));
     await menuContentPage.goToTShirtMenu();
-    await(browser.sleep(3000));
+    await(browser.sleep(5000));
     await productListPage.selectFirstItem();
-    await(browser.sleep(3000));
+    await(browser.sleep(5000));
     await productAddedModalPage.proceedToCheckout();
-    await(browser.sleep(3000));
+    await(browser.sleep(5000));
     await summaryStepPage.proceedToCheckout();
-    await(browser.sleep(3000));
+    await(browser.sleep(5000));
     await signInStepPage.login('aperdomobo@gmail.com', 'WorkshopProtractor');
-    await(browser.sleep(3000));
+    await(browser.sleep(5000));
     await addressStepPage.proceedToCheckout();
-    await(browser.sleep(3000));
+    await(browser.sleep(5000));
 
     await shippingStepPage.acceptAndContinue();
-    await (browser.sleep(3000));
+    await (browser.sleep(5000));
 
     await paymentStepPage.payByBankWire();
-    await (browser.sleep(3000));
+    await (browser.sleep(5000));
     await bankPaymentPage.confirmOrder();
-    await (browser.sleep(3000));
+    await (browser.sleep(5000));
 
     await expect(orderSummaryPage.getOrderTitle()).toBe('Your order on My Store is complete.');
   });
